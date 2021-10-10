b55aac3fb0ef8698da2d8ddc09e76e99f6b4e906
Antti <anttiparhiala@hotmail.com>
2021-10-10 18:03:29 +0300

    siirto

--- Harjoitus/SayNumbers.cpp
+++ Harjoitus/SayNumbers.cpp
@@ -2,25 +2,68 @@
 
 Say::Say()
 {
-	ZeroToNine = { {0, "Zero"}, {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"},
-	{6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"} };
+	ZeroToNine = { {0, ""}, {1, "One "}, {2, "Two "}, {3, "Three "}, {4, "Four "}, {5, "Five "},
+	{6, "Six "}, {7, "Seven "}, {8, "Eight "}, {9, "Nine "} };
 
-	TenToNineteen = { {10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, {14, "Fourteen"}, {15, "Fiftteen"},
-	{16, "Sixteen"}, {17, "Seventeen"}, {18, "Eighteen"}, {19, "Nineteen"} };
+	TenToNineteen = { {10, "Ten "}, {11, "Eleven "}, {12, "Twelve "}, {13, "Thirteen "}, {14, "Fourteen "}, {15, "Fiftteen "},
+	{16, "Sixteen "}, {17, "Seventeen "}, {18, "Eighteen "}, {19, "Nineteen "} };
 	
-	Tens = { {20, "Twenty"}, {30, "Thirty"}, {40, "Fourty"}, {50, "Fifty"}, {60, "Sixty"}, {70, "Seventy"},
-	{80, "Eighty"}, {90, "Ninety"} };
+	Tens = { {2, "Twenty "}, {3, "Thirty "}, {4, "Fourty "}, {5, "Fifty "}, {6, "Sixty "}, {7, "Seventy "},
+	{8, "Eighty "}, {9, "Ninety "} };
+
+	all = { {0, "Zero"}, {1, "One "}, {2, "Two "}, {3, "Three "}, {4, "Four "}, {5, "Five "},
+	{6, "Six "}, {7, "Seven "}, {8, "Eight "}, {9, "Nine "},
+	{10, "Ten "}, {11, "Eleven "}, {12, "Twelve "}, {13, "Thirteen "}, {14, "Fourteen "}, {15, "Fiftteen "},
+	{16, "Sixteen "}, {17, "Seventeen "}, {18, "Eighteen "}, {19, "Nineteen "},
+	{20, "Twenty "}, {30, "Thirty "}, {40, "Fourty "}, {50, "Fifty "}, {60, "Sixty "}, {70, "Seventy "},
+	{80, "Eighty "}, {90, "Ninety "} };
+
+	test = all;
+
+	for (int t = 20; t < 100; t += 10)
+	{
+		for (int i = 1; i < 10; i++)
+		{
+			int ti = t + i;
+			std::string ka = all.find(t)->second + ZeroToNine.find(i)->second;
+			test.emplace(ti, ka);
+		}
+	}
+
 }
 
 void Say::Procces()
 {
 	int num;
+	int maxnum = 10000000;
+	std::string chunks;
 	std::cout << "Enter a number: " << std::endl;
 	std::cin >> num;
+
 	if (std::cin.good())
 	{
-		std::cout << Translate(num) << std::endl;
-		std::cout << '\n';
+		if (num > maxnum)
+		{
+			std::cout << "Number out of range" << std::endl;
+			std::cout << '\n';
+		}
+		else
+		{
+			chunks = std::to_string(num);
+			if (chunks.size() > 3)
+			{
+				for (int i = int(chunks.size() - 3); i > 0; i -= 3)
+				{
+					chunks.insert(i, ",");
+				}
+			}
+
+			std::cout << '\n';
+			std::cout << chunks << std::endl;
+			std::cout << Translate(num) << std::endl;
+			std::cout << '\n';
+		}
+
 	}
 	else
 	{
@@ -33,17 +76,164 @@ void Say::Procces()
 
 std::string Say::Translate(int input)
 {
-	std::string output;
-	std::string Negative = "Minus";
+	std::string out;
 
-	if (input > 0 && input < 10 )
+	int numcount = input / 1000000;
+	input %= 1000000;
+
+	if (numcount > 0)
 	{
-		output = ZeroToNine.find(input)->second;
-	}
-	else if (input >= 10 && input < 20)
-	{
-		output = TenToNineteen.find(input)->second;
+		out += all.find(numcount)->second;
+		out += "Million ";
 	}
 
-	return output;
+	numcount = input / 100000;
+	input %= 100000;
+
+	if (numcount > 0)
+	{
+		out += all.find(numcount)->second;
+		out += "Hundred ";
+	}
+
+	numcount = input / 1000;
+	input %= 1000;
+
+	if (numcount > 0)
+	{
+		out += test.find(numcount)->second;
+		out += "Thousands ";
+	}
+
+	numcount = input / 100;
+	input %= 100;
+
+	if (numcount > 0)
+	{
+		out += all.find(numcount)->second;
+		out += "Hundred ";
+	}
+
+	numcount = input / 1;
+	input %= 1;
+
+	if (numcount > 0)
+	{
+		out += test.find(numcount)->second;
+	}
+
+	return out;
+} 
+
+std::string Say::StringParser(int input)  //String parsing algo, handles translations based on the number and pos of the digits
+{
+	std::string in = std::to_string(input);
+	std::string out;
+	int temp = 0;   //transfer val to the next loop iteration
+
+	for (int i = int(in.size()) - 1; i >= 0; i--)		//handles digit right to left
+	{
+		int t = 0;
+		t = in[i] - '0';
+		if (i == in.size() - 1) //ones
+		{
+			if (in.size() > 1)
+			{
+				temp = t;
+			}
+			else
+			{
+				out.insert(0, ZeroToNine.find(t)->second);
+				if (t == 0)
+				{
+					out = "Zero";
+				}
+			}
+		}
+		else if (i == in.size() - 2)	//tens
+		{
+			if (t == 1)
+			{
+				temp += 10;
+				out.insert(0, TenToNineteen.find(temp)->second);
+			}
+			else
+			{
+				out.insert(0, ZeroToNine.find(temp)->second);
+				if (t != 0)
+				{
+					out.insert(0, Tens.find(t)->second);
+				}
+			}
+			temp = 0;
+		}
+		else if (i == in.size() - 3)	//hundreds
+		{
+			if (t != 0)
+			{
+				out.insert(0, "Hundred ");
+				out.insert(0, ZeroToNine.find(t)->second);
+			}
+		}
+		else if (i == in.size() - 4)  //Thousands
+		{
+			if (temp != 0)
+			{
+				out.insert(0, ZeroToNine.find(temp)->second);
+			}
+			out.insert(0, "Thousand ");
+			if (in.size() > 4)
+			{
+				temp = t;
+			}
+			else
+			{
+				out.insert(0, ZeroToNine.find(t)->second);
+			}
+		}
+		else if (i == in.size() - 5) //Tens of Thousands
+		{
+			if (t == 1)
+			{
+				temp += 10;
+				out.insert(0, TenToNineteen.find(temp)->second);
+			}
+			else
+			{
+				out.insert(0, ZeroToNine.find(temp)->second);
+				if (t != 0)
+				{
+					out.insert(0, Tens.find(t)->second);
+				}
+			}
+		}
+		else if (i == in.size() - 6) //Hundreds of Thousands
+		{
+			if (t != 0)
+			{
+				out.insert(0, "Hundred ");
+				out.insert(0, ZeroToNine.find(t)->second);
+			}
+		}
+	}
+
+	return out;
+}
+
+
+std::string Say::TranslateTestRec(int input, int dive)
+{
+	std::string out;
+
+	int numcount = input / dive;
+	input %= 1000000;
+
+	if (numcount > 0)
+	{
+		out += all.find(numcount)->second;
+		out += "Million ";
+	}
+
+
+	return out;
 }
--- Harjoitus/SayNumbers.h
+++ Harjoitus/SayNumbers.h
@@ -13,9 +13,13 @@ public:
 
 private:
 	std::string Translate(int input);
+	std::string StringParser(int input);
+	std::string TranslateTestRec(int input, int dive);
 
 private:
 	std::map<int, std::string> ZeroToNine;
 	std::map<int, std::string> TenToNineteen;
 	std::map<int, std::string> Tens;
+	std::map<int, std::string> all;
+	std::map<int, std::string> test;
 };
